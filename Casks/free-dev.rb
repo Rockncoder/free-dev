a308604e1fb9e071779272923e59a7e9fbf56152
RocknCoder <rockncoder@gmail.com>
2026-07-06 11:43:20 -0700

    cask: drop unnecessary verified: parameter

--- Casks/free-dev.rb
+++ Casks/free-dev.rb
@@ -2,8 +2,7 @@ cask "free-dev" do
   version "1.0.0"
   sha256 "fe941d8d26375f35dcc88032cf335c69603c8eb1bd81f6ad80f17a72cbc3a4e1"
 
-  url "https://github.com/Rockncoder/free-dev/releases/download/v#{version}/FreeDev.dmg",
-      verified: "github.com/Rockncoder/free-dev/"
+  url "https://github.com/Rockncoder/free-dev/releases/download/v#{version}/FreeDev.dmg"
   name "Free Dev"
   desc "Menu bar app that reclaims disk space from Xcode and dev-tool leftovers"
   homepage "https://github.com/Rockncoder/free-dev"
